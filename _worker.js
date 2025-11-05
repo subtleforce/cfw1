ed37013ee8de05a9f9397e01e72314055e1bf167
subtleforce <subtleforcerise@proton.me>
2025-11-05 09:28:08 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,6 +2,6 @@ export default {
   async fetch(request, env, ctx) {
     // You can view your logs in the Observability dashboard
     console.info({ message: 'Hello World Worker make by github' }); 
-    return new Response('Hello World from github V2');
+    return new Response('Hello World from github V3 ..');
   }
 };
