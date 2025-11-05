5c62ae75edd84529605986a0d3f111ae8a9dfb59
subtleforce <subtleforcerise@proton.me>
2025-11-05 01:03:03 -0500

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,7 @@
+export default {
+  async fetch(request, env, ctx) {
+    // You can view your logs in the Observability dashboard
+    console.info({ message: 'Hello World Worker make by github' }); 
+    return new Response('Hello World from github!');
+  }
+};
